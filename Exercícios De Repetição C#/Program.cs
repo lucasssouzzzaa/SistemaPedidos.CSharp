35b14889bcab6e6eaa700c0b1dd4b3dda99cd70d
FIC <fic@info.local>
2026-04-29 17:24:52 -0300

    Exercícios de Repetição

---
+++ Exercícios De Repetição C#/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Exercícios_De_Repetição_C_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Ex 1
+        // Console.WriteLine("1 até 10: ");
+        // for (int i = 1; i <= 10; i++)
+        // {
+        //     Console.Write(i + " ");
+        // }
+
+        //Ex 2
+        // Console.WriteLine("10 até 1: ");
+        // for (int i = 10; i >= 1; i--)
+        // {
+        //     Console .WriteLine(i + " ");
+        // }
+
+        //Ex 3 
+        // Console.Write("Digite um número: ");
+        // int numero = int.Parse(Console.ReadLine());
+        // for (int i = 1; i <= 10; i++)
+        // {
+        //     Console.WriteLine(numero + " x " + i + " = " + (numero * i));
+        // }
+
+        // Ex 4
+        // Console.WriteLine("até 5 (while):");
+        // int contador = 1;
+        // while (contador <= 5)
+        // {
+        //     Console.Write(contador + " ");
+        //     contador++;
+        // }
+
+        // Ex 5 
+        //    int soma = 0;
+        // for (int i = 1; i <= 5; i++)
+        //     soma += i;
+        // Console.WriteLine("\n\nSoma de 1 a 5: " + soma);
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        // Ex 1 médio
+        // int soma2 = 0;
+        // for (int i = 1; i <= 5; i++)
+        // {
+        //     Console.Write("Digite um número: ");
+        //     int n = int.Parse(Console.ReadLine());
+        //     soma2 += n;
+        // }
+        // Console.WriteLine("Soma total: " + soma2);
+
+        // Ex 2 médio
+        // Console.WriteLine("Números pares de 1 a 20:");
+        // for (int i = 1; i <= 20; i++)
+        // {
+        //     if (i % 2 == 0)
+        //         Console.Write(i + " ");
+        // }
+
+        // Ex 3
+        // string senha;
+        // do
+        // {
+        //     Console.Write("Digite a senha: ");
+        //     senha = Console.ReadLine();
+        // } while (senha != "1234");
+        // Console.WriteLine("Acesso permitido");
+
+        // Ex 4
+        // int opcao;
+        // do
+        // {
+        //     Console.WriteLine("Menu:");
+        //     Console.WriteLine("1 - Mostrar mensagem");
+        //     Console.WriteLine("2 - Sair");
+        //     Console.Write("Escolha: ");
+        //     opcao = int.Parse(Console.ReadLine());
+
+        //     if (opcao == 1)
+        //         Console.WriteLine("Olá, usuário!");
+        //     else if (opcao != 2)
+        //         Console.WriteLine("Opção inválida");
+
+        // } while (opcao != 2);
+
+        // // Ex 5
+        // double soma3 = 0;
+        // for (int i = 1; i <= 4; i++)
+        // {
+        //     Console.Write("Digite um número: ");
+        //     double valor = double.Parse(Console.ReadLine());
+        //     soma3 += valor;
+        // }
+        // double media = soma3 / 4;
+        // Console.WriteLine("Média: " + media);
+
+        // Console.ReadKey();
+    }
+}
